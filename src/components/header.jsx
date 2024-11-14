8a155c0a02515a2d3c196bf49f6f6eddc18270ab
Navjot Singh <109858950+navjot369@users.noreply.github.com>
2024-11-14 20:25:47 +0530

    Add basic header

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+    return(<div className="fixed m-0 px-6 py-4 flex justify-between w-full items-center">
+        <h1 className="text-2xl font-bold">Temperature Converter</h1>
+        <div className="">
+        <button></button>
+        <a href="https://github.com/navjot369/Temp-Converter" className="text-lg bg-primary px-4 py-2 rounded-full">Github</a> 
+        </div>
+        </div>);
+}
